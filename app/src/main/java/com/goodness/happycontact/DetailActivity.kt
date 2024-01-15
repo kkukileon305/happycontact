d97bdf171b387f28a67d0b2d368c1bf7c5bbd377
kkukileon305 <kkukileon305@gmail.com>
2024-01-15 14:34:50 +0900

    init

---
+++ app/src/main/java/com/goodness/happycontact/DetailActivity.kt
@@ -0,0 +1,14 @@
+package com.goodness.happycontact
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.goodness.happycontact.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+	private val binding by lazy { ActivityDetailBinding.inflate(layoutInflater) }
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(binding.root)
+	}
+}
---
+++ app/src/main/java/com/goodness/happycontact/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.goodness.happycontact
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.goodness.happycontact.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+	private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(binding.root)
+	}
+}
