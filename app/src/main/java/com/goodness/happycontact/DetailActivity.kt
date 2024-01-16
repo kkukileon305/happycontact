88e874aecb29ccd35d2a5d04ca38bd013b4d8d1c
hyunparrot <coyh1071@gmail.com>
2024-01-16 19:25:42 +0900

    bundle 조금 구현

--- app/src/main/java/com/goodness/happycontact/DetailActivity.kt
+++ app/src/main/java/com/goodness/happycontact/DetailActivity.kt
@@ -10,5 +10,16 @@ class DetailActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(binding.root)
+
+
+		val receivedIntent = intent
+		val receivedBundle = receivedIntent.extras
+
+		if (receivedBundle != null) {
+			val profileImage = receivedBundle.getInt("profileImage", 0)
+			val name = receivedBundle.getString("name", "")
+			val like = receivedBundle.getBoolean("like", false)
+			val phoneNumber = receivedBundle.getString("phoneNumber", "")
+		}
 	}
 }
--- app/src/main/java/com/goodness/happycontact/MainActivity.kt
+++ app/src/main/java/com/goodness/happycontact/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.goodness.happycontact
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.goodness.happycontact.databinding.ActivityMainBinding
@@ -10,5 +11,19 @@ class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(binding.root)
+
+
+
+
+		val bundle = Bundle().apply {
+			putInt("profileImage", R.drawable.break_sparta)
+			putString("name", "파괴하는 르탄이")
+			putBoolean("like", false)
+			putString("phoneNumber", "01012341234")
+		}
+
+		val intent = Intent(this, DetailActivity::class.java)
+		intent.putExtras(bundle)
+		startActivity(intent)
 	}
 }
