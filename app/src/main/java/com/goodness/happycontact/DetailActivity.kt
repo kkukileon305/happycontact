8f6cfcbb52c4f28ff1df56c33d766c0ab7bd1703
eunmyeungjun <audwns419@gmail.comm>
2024-01-17 10:45:51 +0900

    merge오류 수정

--- app/src/main/java/com/goodness/happycontact/DetailActivity.kt
+++ app/src/main/java/com/goodness/happycontact/DetailActivity.kt
@@ -1,7 +1,7 @@
 package com.goodness.happycontact
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.goodness.happycontact.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
@@ -11,9 +11,24 @@ class DetailActivity : AppCompatActivity() {
 		super.onCreate(savedInstanceState)
 		setContentView(binding.root)
 
-
 		val receivedIntent = intent
 		val receivedBundle = receivedIntent.extras
 
+
+		// Bundle에서 연락처 정보 추출
+		val contact = receivedBundle?.getParcelable<Contact>(Contact.CONTACT_KEY)
+
+		// 연락처 정보가 null이 아닌 경우에만 표시
+		if (contact != null) {
+			// 연락처 정보를 화면에 표시하는 작업 수행
+
+			binding.textView.text = "${contact.name}"
+			binding.tvNumber.text = "${contact.phoneNumber}"
+			binding.tvNumber2.text = "${contact.email}"
+
+
+			val profileImageView = binding.imageView
+			profileImageView.setImageResource(contact.profileImage)
+		}
 	}
 }
--- app/src/main/java/com/goodness/happycontact/MainActivity.kt
+++ app/src/main/java/com/goodness/happycontact/MainActivity.kt
@@ -13,80 +13,105 @@ import de.hdodenhof.circleimageview.CircleImageView
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
-	private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-	private val dataList = mutableListOf<Contact>()
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val dataList = mutableListOf<Contact>()
 
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		setContentView(binding.root)
 
-		binding.btnTest.setOnClickListener {
-			val builder = AlertDialog.Builder(this)
-			builder.setTitle("Add contact")
 
-			val view1 = layoutInflater.inflate(R.layout.add_contact_dialog, null)
-			builder.setView(view1)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
 
-			val listener = DialogInterface.OnClickListener { dialog, _ ->
-				val alert = dialog as AlertDialog
+
+
+        val receivedIntent = intent
+        val receivedBundle = receivedIntent.extras
+
+
+        binding.btnTest.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Add contact")
+
+            val view1 = layoutInflater.inflate(R.layout.add_contact_dialog, null)
+            builder.setView(view1)
+
+            val listener = DialogInterface.OnClickListener { dialog, _ ->
+                val alert = dialog as AlertDialog
 
 //				val rep1: CircleImageView? = alert.findViewById(R.id.editImg) // 이미지 선택 구현 후 부여 필요함..1
-				val rep1 = findViewById<CircleImageView>(R.id.editImg)
-				val rep2: EditText? = alert.findViewById(R.id.editName)
-				val rep3: EditText? = alert.findViewById(R.id.editNum)
-				val rep4: EditText? = alert.findViewById(R.id.editMail)
+                val rep1 = findViewById<CircleImageView>(R.id.editImg)
+                val rep2: EditText? = alert.findViewById(R.id.editName)
+                val rep3: EditText? = alert.findViewById(R.id.editNum)
+                val rep4: EditText? = alert.findViewById(R.id.editMail)
 
 //				val img = findViewById<CircleImageView>(R.id.editImg) // 이미지 선택 구현 후 부여 필요함..2
-				val img = rep1.toString().toInt()
-				val name = rep2?.text.toString()
-				val phoneNumber = rep3?.text.toString()
-				val email = rep4?.text.toString()
+                val img = rep1.toString().toInt()
+                val name = rep2?.text.toString()
+                val phoneNumber = rep3?.text.toString()
+                val email = rep4?.text.toString()
 
-				dataList.add(Contact(dataList.size + 1, img, name, email, phoneNumber, false))
-			}
+                dataList.add(Contact(dataList.size + 1, img, name, email, phoneNumber, false))
 
-			builder.setPositiveButton("추가") { dialog, _ ->
-				// 버튼 클릭시 실행될 로직
-				listener
-				Validation(view1, dialog)
-			}
+                //정현우 작업물 디테일에 데이터 보내는 작업.
+                val dtContact = Contact(dataList.size + 1, img, name, email, phoneNumber, false)
+                dataList.add(dtContact)
 
-			builder.setNegativeButton("취소", null)
-			builder.show()
-		}
-		setSwiper()
-	}
+                val intent = Intent(this@MainActivity, DetailActivity::class.java)
 
-	private fun setSwiper() {
-		val viewPager = binding.pager
-		val tabLayout = binding.tabLayout
+                // Bundle에 연락처 정보 추가
+                val bundle = Bundle()
+                bundle.putParcelable(Contact.CONTACT_KEY, dtContact)
+                intent.putExtras(bundle)
 
-		viewPager.adapter = MainAdapter(this)
+                // 디테일 페이지 시작
+                startActivity(intent)
+            }
+            //정현우 작업 끝
 
-		TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-			tab.text = when (position) {
-				0 -> getString(R.string.main_tab_name_1)
-				1 -> getString(R.string.main_tab_name_2)
-				else -> ""
-			}
-		}.attach()
-	}
+            builder.setPositiveButton("추가") { dialog, _ ->
+                // 버튼 클릭시 실행될 로직
+                listener
+                Validation(view1, dialog)
+            }
 
-	private fun Validation(dialogView: View, dialog: DialogInterface) {
-		val rep2: EditText? = dialogView.findViewById(R.id.editName)
-		val rep3: EditText? = dialogView.findViewById(R.id.editNum)
-		val rep4: EditText? = dialogView.findViewById(R.id.editMail)
+            builder.setNegativeButton("취소", null)
+            builder.show()
+        }
+        setSwiper()
+    }
 
-		val name = rep2?.text.toString()
-		val phoneNumber = rep3?.text.toString()
-		val email = rep4?.text.toString()
+    private fun setSwiper() {
+        val viewPager = binding.pager
+        val tabLayout = binding.tabLayout
 
-		if (name.isEmpty() || phoneNumber.isEmpty() || email.isEmpty()) {
-			Toast.makeText(this, "1개 이상의 정보가 입력되지 않았습니다", Toast.LENGTH_SHORT).show()
-		} else {
-			//적용이 안됨
-		}
-	}
+        viewPager.adapter = MainAdapter(this)
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = when (position) {
+                0 -> getString(R.string.main_tab_name_1)
+                1 -> getString(R.string.main_tab_name_2)
+                else -> ""
+            }
+        }.attach()
+    }
+
+    private fun Validation(dialogView: View, dialog: DialogInterface) {
+        val rep2: EditText? = dialogView.findViewById(R.id.editName)
+        val rep3: EditText? = dialogView.findViewById(R.id.editNum)
+        val rep4: EditText? = dialogView.findViewById(R.id.editMail)
+
+        val name = rep2?.text.toString()
+        val phoneNumber = rep3?.text.toString()
+        val email = rep4?.text.toString()
+
+        if (name.isEmpty() || phoneNumber.isEmpty() || email.isEmpty()) {
+            Toast.makeText(this, "1개 이상의 정보가 입력되지 않았습니다", Toast.LENGTH_SHORT).show()
+        } else {
+            //적용이 안됨
+        }
+    }
+
 
 }
+
