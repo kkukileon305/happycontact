ae16d839b8367fc083ca5cab91b45ce2c7c34bbc
eunmyeungjun <audwns419@gmail.comm>
2024-01-17 15:47:14 +0900

    생일 입력받을 때 예외로 다운억제

--- app/src/main/java/com/goodness/happycontact/MyPage.kt
+++ app/src/main/java/com/goodness/happycontact/MyPage.kt
@@ -9,10 +9,13 @@ import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.ImageView
 import androidx.appcompat.app.AlertDialog
-import com.goodness.happycontact.databinding.FragmentContactListBinding
 import com.goodness.happycontact.databinding.FragmentMyPageBinding
 import com.goodness.happycontact.databinding.MypageEditDialogBinding
+import java.lang.Exception
 import java.text.DecimalFormat
+import java.text.SimpleDateFormat
+import java.util.Locale
+
 
 class MyPage : Fragment() {
 	private lateinit var binding: FragmentMyPageBinding
@@ -36,13 +39,20 @@ class MyPage : Fragment() {
 		binding.ivEdit.setOnClickListener {
 			val builder = AlertDialog.Builder(requireContext())
 
-
 			val dialogBinding = MypageEditDialogBinding.inflate(layoutInflater)
-			builder.setView(dialogBinding.root)
+
+
 			val editName: EditText = dialogBinding.dialogEditName
 			val editEmail: EditText = dialogBinding.dialogEditEmail
 			val editNumber: EditText = dialogBinding.dialogEditNumber
 			val profileImage : ImageView = dialogBinding.dialogNowProfileImage
+			val editBirth : EditText = dialogBinding.dialogEditBirthDay
+
+			//현재 설정한 정보 다이얼로그 Edithint에 띄우기
+			editName.hint = (binding.tvMyName).text
+			editEmail.hint = (binding.tvMyEmail).text
+			editNumber.hint = (binding.tvMyPhoneNum).text
+			builder.setView(dialogBinding.root)
 
 			dialogBinding.ivEditProfileImage.setOnClickListener {
 				val intent = Intent(Intent.ACTION_PICK)
@@ -50,16 +60,12 @@ class MyPage : Fragment() {
 				startActivity(intent)
 			}
 
+//			val numFormat = DecimalFormat("###-####-####")
+//			val rawPhoneNumber = (binding.tvMyPhoneNum.text).toString()
+//			val formattingNum = numFormat.format(rawPhoneNumber)
 
 
-			//현재 설정한 이름 다이얼로그 Edithint에 띄우기
-			editName.hint = (binding.tvMyName).text
-			editEmail.hint = (binding.tvMyEmail).text
-			editNumber.hint = (binding.tvMyPhoneNum).text
-
-			builder.setPositiveButton("변경") { _,_ ->
-
-
+			builder.setPositiveButton("변경") { _, _ ->
 
 				if (editName.text.isNotEmpty()) {
 					binding.tvMyName.text = (editName.text).toString()
@@ -69,7 +75,21 @@ class MyPage : Fragment() {
 				}
 				if (editNumber.text.isNotEmpty()) {
 					binding.tvMyPhoneNum.text = (editNumber.text).toString()
+				}
+				try {
+					if (editBirth.text.isNotEmpty()) {
+						val enterDate = editBirth.text.toString()
+						val dateFormat = SimpleDateFormat("yyyy/MM/dd", Locale.getDefault())
+						val parsedDate = dateFormat.parse(enterDate)
 
+						if (parsedDate != null) {
+							val formattedDate = dateFormat.format(parsedDate)
+							binding.tvMyBirthDay.text = formattedDate
+						}
+
+					}
+				} catch (e: Exception){
+					e.printStackTrace()
 				}
 			}
 
