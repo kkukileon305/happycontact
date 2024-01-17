54486b0755c36725f2fe703078fdcf145cfd8efa
Box-o <jeffmailkr@gmail.com>
2024-01-17 16:41:20 +0900

    Fix: MainActivity dialog code remove & AddFrag custom dialog code add

--- app/src/main/java/com/goodness/happycontact/MainActivity.kt
+++ app/src/main/java/com/goodness/happycontact/MainActivity.kt
@@ -9,52 +9,19 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.goodness.happycontact.databinding.ActivityMainBinding
+import com.goodness.happycontact.databinding.AddContactDialogBinding
 import de.hdodenhof.circleimageview.CircleImageView
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 	private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-	private val dataList = mutableListOf<Contact>()
 
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(binding.root)
-		
-		binding.btnTest.setOnClickListener {
-			val builder = AlertDialog.Builder(this)
-			builder.setTitle("Add contact")
 
-			val view1 = layoutInflater.inflate(R.layout.add_contact_dialog, null)
-			builder.setView(view1)
-
-			val listener = DialogInterface.OnClickListener { dialog, _ ->
-				val alert = dialog as AlertDialog
-
-//				val rep1: CircleImageView? = alert.findViewById(R.id.editImg) // 이미지 선택 구현 후 부여 필요함..1
-				val rep1 = findViewById<CircleImageView>(R.id.editImg)
-				val rep2: EditText? = alert.findViewById(R.id.editName)
-				val rep3: EditText? = alert.findViewById(R.id.editNum)
-				val rep4: EditText? = alert.findViewById(R.id.editMail)
-
-//				val img = findViewById<CircleImageView>(R.id.editImg) // 이미지 선택 구현 후 부여 필요함..2
-				val img = rep1.toString().toInt()
-				val name = rep2?.text.toString()
-				val phoneNumber = rep3?.text.toString()
-				val email = rep4?.text.toString()
-
-				dataList.add(Contact(dataList.size + 1, img, name, email, phoneNumber, false))
-			}
-
-			builder.setPositiveButton("추가") { dialog, _ ->
-				// 버튼 클릭시 실행될 로직
-				listener
-				Validation(view1, dialog)
-			}
-
-			builder.setNegativeButton("취소", null)
-			builder.show()
-		}
+		addButtonPressed()
 		setSwiper()
 	}
 
@@ -73,7 +40,16 @@ class MainActivity : AppCompatActivity() {
 		}.attach()
 	}
 
-	private fun Validation(dialogView: View, dialog: DialogInterface) {
+	private fun addButtonPressed(){
+		binding.btnTest.setOnClickListener {
+			AddFrag().show(supportFragmentManager,"AddFrag")
+
+		}
+	}
+
+
+
+/*	private fun Validation(dialogView: View, dialog: DialogInterface) {
 		val rep2: EditText? = dialogView.findViewById(R.id.editName)
 		val rep3: EditText? = dialogView.findViewById(R.id.editNum)
 		val rep4: EditText? = dialogView.findViewById(R.id.editMail)
@@ -87,6 +63,6 @@ class MainActivity : AppCompatActivity() {
 		} else {
 			//적용이 안됨
 		}
-	}
+	}*/
 
 }
