8b164fa8152c73aa23c251ba61fbc9e8c1e13edb
hyunparrot <coyh1071@gmail.com>
2024-01-19 11:16:40 +0900

    Add HeartClickListener ------------------------
    Feat: add data (Contact), add AddFrag(addNewContact)
    
    ------------------------
    <꼬리말>

--- app/src/main/java/com/goodness/happycontact/Contact.kt
+++ app/src/main/java/com/goodness/happycontact/Contact.kt
@@ -18,11 +18,11 @@ data class Contact(
 	val email: String,
 	val phoneNumber: String,
 	var like: Boolean,
-//	val address: String,
-//	val relationship: String,
-//	val bigDay: String,
-//	val birthDay: String
-//	val memo: String
+	val address: String,
+	val relationship: String,
+	val bigDay: String,
+	val birthDay: String,
+	val memo: String
 
 ) : Parcelable {
 	companion object {
@@ -33,7 +33,12 @@ data class Contact(
 				name = "파괴하는 르탄이",
 				email = "break@sparta.com",
 				like = false,
-				phoneNumber = "01012341234"
+				phoneNumber = "01012341234",
+				address = "스파시 르타구 코딩동 클럽번지",
+				relationship = "직장상사",
+				bigDay = "1.19 회식",
+				birthDay = "10월19일",
+				memo = "전화 오면 무시"
 			),
 			Contact(
 				id = 2,
@@ -41,7 +46,12 @@ data class Contact(
 				name = "치킨르탄",
 				email = "chicken@sparta.com",
 				like = false,
-				phoneNumber = "01078782341"
+				phoneNumber = "01078782341",
+				address = "스파시 르타구 코딩동 클럽번지",
+				relationship = "직장동료",
+				bigDay = "10.14 프로젝트 진행",
+				birthDay = "12월31일",
+				memo = " "
 			),
 			Contact(
 				id = 3,
@@ -49,7 +59,12 @@ data class Contact(
 				name = "용탄이",
 				email = "dragon@sparta.com",
 				like = false,
-				phoneNumber = "01032121234"
+				phoneNumber = "01032121234",
+				address = "스파시 르타구 코딩동 클럽번지",
+				relationship = "직장동료",
+				bigDay = "10.01 입사",
+				birthDay = "6월20일",
+				memo = "신입"
 			),
 			Contact(
 				id = 4,
@@ -57,7 +72,12 @@ data class Contact(
 				name = "뿔난르탄이",
 				email = "horn@sparta.com",
 				like = false,
-				phoneNumber = "01088888888"
+				phoneNumber = "01088888888",
+				address = "스파시 르타구 코딩동 클럽번지",
+				relationship = "친구",
+				bigDay = "7.14 일본여행",
+				birthDay = "7월14일",
+				memo = "베프"
 			),
 			Contact(
 				id = 5,
@@ -65,7 +85,12 @@ data class Contact(
 				name = "원르탄이",
 				email = "monkey@sparta.com",
 				like = false,
-				phoneNumber = "01064646464"
+				phoneNumber = "01064646464",
+				address = "스파시 르타구 코딩동 클럽번지",
+				relationship = "친구",
+				bigDay = "1.20 약속",
+				birthDay = "3월26일",
+				memo = "약속 시간 안지킴"
 			),
 			Contact(
 				id = 6,
@@ -73,7 +98,12 @@ data class Contact(
 				name = "영화보는 르탄이",
 				email = "moviegood@sparta.com",
 				like = false,
-				phoneNumber = "01081238123"
+				phoneNumber = "01081238123",
+				address = "스파시 르타구 코딩동 클럽번지",
+				relationship = "코딩 스터디 일원",
+				bigDay = "1.24 스터디 진행",
+				birthDay = "12월01일",
+				memo = "스터디 일원"
 			),
 			Contact(
 				id = 7,
@@ -81,7 +111,12 @@ data class Contact(
 				name = "토끼르탄",
 				email = "rabbit@sparta.com",
 				like = false,
-				phoneNumber = "01011111111"
+				phoneNumber = "01011111111",
+				address = "스파시 르타구 코딩동 클럽번지",
+				relationship = "코딩 스터디 조장",
+				bigDay = "1.24 스터디 진행",
+				birthDay = "10월20일",
+				memo = "스터디 조장"
 			),
 			Contact(
 				id = 8,
@@ -89,7 +124,12 @@ data class Contact(
 				name = "도망가는 르탄이",
 				email = "runsparta@sparta.com",
 				like = false,
-				phoneNumber = "01066666666"
+				phoneNumber = "01066666666",
+				address = "스파시 르타구 코딩동 클럽번지",
+				relationship = "코딩 스터디 일원",
+				bigDay = "1.24 스터디 진행",
+				birthDay = "3월8일",
+				memo = " "
 			),
 			Contact(
 				id = 9,
@@ -97,7 +137,12 @@ data class Contact(
 				name = "양탄이",
 				email = "sheep@sparta.com",
 				like = false,
-				phoneNumber = "01077777777"
+				phoneNumber = "01077777777",
+				address = "스파시 르타구 코딩동 클럽번지",
+				relationship = "코딩 스터디 일원",
+				bigDay = "1.24 스터디 진행",
+				birthDay = "1월5일",
+				memo = "전화 오면 무시"
 			),
 			Contact(
 				id = 10,
@@ -105,7 +150,12 @@ data class Contact(
 				name = "뱀탄",
 				email = "snake@sparta.com",
 				like = false,
-				phoneNumber = "01043214321"
+				phoneNumber = "01043214321",
+				address = "스파시 르타구 코딩동 클럽번지",
+				relationship = "스파르타 매니저님",
+				bigDay = "1.23 발제",
+				birthDay = "7월3일",
+				memo = " "
 			),
 			Contact(
 				id = 11,
@@ -113,7 +163,12 @@ data class Contact(
 				name = "그냥르탄",
 				email = "sparta@sparta.com",
 				like = false,
-				phoneNumber = "01000001234"
+				phoneNumber = "01000001234",
+				address = "스파시 르타구 코딩동 클럽번지",
+				relationship = "스파르타 매니저님",
+				bigDay = " ",
+				birthDay = "4월13일",
+				memo = "전화 바로 받기"
 			),
 			Contact(
 				id = 12,
@@ -121,7 +176,12 @@ data class Contact(
 				name = "행복한 르탄",
 				email = "happy@sparta.com",
 				like = false,
-				phoneNumber = "01043214321"
+				phoneNumber = "01043214321",
+				address = "스파시 르타구 코딩동 클럽번지",
+				relationship = "스파르타 튜터님",
+				bigDay = " ",
+				birthDay = "2월8일",
+				memo = "오전에만 계심"
 			),
 			Contact(
 				id = 13,
@@ -129,7 +189,12 @@ data class Contact(
 				name = "개탄",
 				email = "dogtan@sparta.com",
 				like = false,
-				phoneNumber = "01099999999"
+				phoneNumber = "01099999999",
+				address = "스파시 르타구 코딩동 클럽번지",
+				relationship = "스파르타 튜터님",
+				bigDay = "1.14 줌 수업",
+				birthDay = "6월13일",
+				memo = "챌린지 튜터님"
 			),
 			Contact(
 				id = 14,
@@ -137,7 +202,12 @@ data class Contact(
 				name = "호랑탄",
 				email = "tiger@sparta.com",
 				like = false,
-				phoneNumber = "01012343213"
+				phoneNumber = "01012343213",
+				address = "스파시 르타구 코딩동 클럽번지",
+				relationship = "스파르타 튜터님",
+				bigDay = "1.18 피드백",
+				birthDay = "12월15일",
+				memo = "스탠다드 튜터님"
 			),
 			Contact(
 				id = 15,
@@ -145,7 +215,12 @@ data class Contact(
 				name = "전탄",
 				email = "warrior@sparta.com",
 				like = false,
-				phoneNumber = "01011112222"
+				phoneNumber = "01011112222",
+				address = "스파시 르타구 코딩동 클럽번지",
+				relationship = "스파르타 튜터님",
+				bigDay = "1.14 줌 수업",
+				birthDay = "8월4일",
+				memo = "베이직 튜터님"
 			),
 		).sortedBy { it.name }.toMutableList()
 
